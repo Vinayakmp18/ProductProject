ca442e4c41bcf69a57270da0622002840ea549da
Vinayakmp18 <126151478+Vinayakmp18@users.noreply.github.com>
2023-03-17 23:53:55 +0530

    Update ProductQuill.java

--- ProductQuill.java
+++ ProductQuill.java
@@ -1,118 +1,123 @@
-package demo;
+package adapt;
 
-import java.io.FileWriter;
-import java.io.IOException;
+import com.opencsv.CSVWriter;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-public class ProductQuill {
-	public static void main(String[] args) {
-		final String url="https://www.quill.com/hanging-file-folders/cbk/122567.html";
-		final String url1="https://www.quill.com/quill-brand-standard-green-100-recycled-hanging-file-folders-1-5-cut-adjustable-tabs-letter-size-50-case/cbs/005880.html";
-		
-		String csvFilePath = "product_details.csv";
-		int count=0; int count1=0; int count2=0; int count3=0; int count4=0; int count5=0;
-		try {
-			FileWriter csvWriter = new FileWriter(csvFilePath);
-            csvWriter.append("Product Name");
-            csvWriter.append(",");
-            csvWriter.append("Product Price");
-            csvWriter.append(",");
-            csvWriter.append("Item Number/SKU/Product Code");
-            csvWriter.append(",");
-            csvWriter.append("Model Number");
-            csvWriter.append(",");
-            csvWriter.append("Product Category");
-            csvWriter.append(",");
-            csvWriter.append("Product Description");
-            csvWriter.append("\n");
-			
-			final Document document=Jsoup.connect(url).get();
-			final Document document1=Jsoup.connect(url1).get();
-			
-			
-			for (Element row : document.select(
-					"div.col-7.py-0.product-name-wrap.js-productSection.col"
-					)) {
-					final String name=row.select(".search-product-name-wrap.d-block.body-xs").text();
-					if(count<10)
-					{
-						csvWriter.append(name);
-			            csvWriter.append("\n");
-						count++;
-					}
-			}
-			csvWriter.append("\n");
-			for (Element row : document.select(
-					"div.col-5.py-0.pricing-section-wrap.js-pricingSection.col"
-					)) { 
-					final String price=row.select(".text-black.mb-0.price-size").text();
-					if(count1<10)
-					{
-						csvWriter.append(price);
-			            csvWriter.append("\n");
-						count1++;
-					}	
-			}
-			csvWriter.append(",");
-			for (Element row : document.select(
-					"div.col-7.py-0.product-name-wrap.js-productSection.col"
-					)) {
-					final String item=row.select(".pr-2.LEffortFindNum.fg-jet-tint.body-xxs.col-auto").text();
-					if(count2<10)
-					{
-						csvWriter.append(item);
-			            csvWriter.append("\n");
-						count2++;
-					}
-			}
-			csvWriter.append(",");
-			for (Element row : document1.select(
-					"div.col-xl-8.col-md-7.col-12"
-					)) {
-					final String model=row.select(".ml-md-2.mb-md-0.mb-2:nth-of-type(2)").text();
-					if(count3<10)
-					{
-						csvWriter.append(model);
-			            csvWriter.append("\n");
-						count3++;
-					}
-			}
-			csvWriter.append(",");
-			for (Element row : document.select(
-					"div.container-lg.px-sm-5.px-3"
-					)) {
-					final String category=row.select("#searchResultsInfoContainer > div.row:nth-of-type(1) > .col").text();
-					if(count4<10)
-					{
-						csvWriter.append(category);
-			            csvWriter.append("\n");
-			            count4++;
-					}	
-			}
-			csvWriter.append(",");
-			for (Element row : document.select(
-					".col-7.py-0.product-name-wrap.js-productSection.col"
-					)) {
-					final String description=row.select(".pb-1.mb-2.body-xxs.js-skuBullets").text();
-					if(count5<10)
-					{
-						csvWriter.append(description);
-						csvWriter.append("\n");
-						count5++;
-					}
-			}
-			csvWriter.flush();
-            csvWriter.close();
-            System.out.println("Product details have been exported to " + csvFilePath);
-			
-		}
-			
-		catch (Exception e) {
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringJoiner;
+
+public class ProductQuill2 {
+
+    final static String BASE_URL = "https://www.quill.com";
+    final static String URL = "https://www.quill.com/hanging-file-folders/cbk/122567.html";
+
+//    final static String SEP_CHAR = ": ";
+
+    public static void main(String[] args) {
+
+        String csvFilePath = "product_details.csv";
+
+        List<String[]> data = new ArrayList();
+
+        try {
+            final Document document= Jsoup.connect(URL).get();
+            List<String> links = top10ProductLinks(document);
+            String category = productCategory(document);
+            String itemCode = "" , modelNumber = "", brand = "";
+            for (String link: links) {
+                Document productDocument = Jsoup.connect(link).get();
+                Element productSku = productDocument.select("div#SkuMainContentDiv").first();
+                String productName = productName(productSku);
+                Element productElements = productSku.select("div.body-xs.d-md-flex.fg-jet-tint").first();
+                for (Element productElement: productElements.children()) {
+                    if (productElement.text().contains("Item")) {
+                        itemCode = productItem(productElement);
+                    } else if (productElement.text().contains("Model")) {
+                        modelNumber = productModel(productElement);
+                    } else if (productElement.text().contains("Brand")) {
+                        brand = productBrand(productElement);
+                    }
+                }
+                String price =  productPrice(productDocument);
+                String description = productDescription(productDocument);
+                data.add(new String[]{productName, price, itemCode, modelNumber, category, description});
+            }
+            writeToCSV(csvFilePath, data);
+        } catch (Exception e) {
 			e.printStackTrace();
 		}
-	}
-}
 
+    }
+
+    private static void writeToCSV(String csvFilePath, List<String[]> data) throws IOException {
+        FileWriter outputfile = new FileWriter(csvFilePath);
+        CSVWriter csvWriter = new CSVWriter(outputfile);
+        String[] header = {"Product Name", "Product Price", "Item Number/SKU/Product Code", "Model Number",
+                "Product Category", "Product Description"};
+        csvWriter.writeNext(header);
+        for (String[] row: data ) {
+            csvWriter.writeNext(row);
+        }
+        csvWriter.close();
+    }
+
+    private static List<String> top10ProductLinks(Document document) {
+        Elements elements = document.select("div.d-flex.flex-row.justify-content-center.product-image-wrap > a");
+        int count = 0;
+        List<String> urls = new ArrayList();
+        for (Element element: elements) {
+            String link = element.attr("href");
+            urls.add(BASE_URL + link);
+            count++;
+            if (count > 9) {
+                break;
+            }
+        }
+        return urls;
+    }
+
+    private static String productName(Element productSku) {
+        Element nameElement = productSku.select("h1.skuName").first();
+        return nameElement.text();
+    }
+
+    private static String productBrand(Element productElement) {
+        int sepIndex = productElement.text().indexOf(": ") + 1;
+        return productElement.text().substring(sepIndex);
+    }
+
+    private static String productPrice(Document productDocument) {
+        Element priceContainer = productDocument.select("div.pricing-wrap").first();
+        Element priceElement = priceContainer.select("div > div > span").first();
+        return priceElement.text();
+    }
+
+    private static String productModel(Element productElement) {
+        int sepIndex = productElement.text().indexOf(": ") + 1;
+        return productElement.text().substring(sepIndex);
+    }
+
+    private static String productItem(Element productElement) {
+        int sepIndex = productElement.text().indexOf(": ") + 1;
+        return productElement.text().substring(sepIndex);
+    }
+
+    private static String productCategory(Document document) {
+        return document.select("div > h1#SearchTerm").first().text();
+    }
+
+    private static String productDescription(Document productDocument) {
+        Elements desElement = productDocument.select("div#skuDescription > div > ul > li");
+        StringJoiner joiner = new StringJoiner("\n");
+        for (Element element: desElement) {
+            joiner.add(element.text());
+        }
+        return joiner.toString();
+    }
+}
